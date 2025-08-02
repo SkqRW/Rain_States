fef59d1f6fe90664c0d7aa813928872a0193e7bf
Skq <skq.rain@gmail.com>
2025-08-01 22:05:28 -0500

    Agregar archivos de proyecto.

---
+++ DevTools.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security;
+using System.Security.Permissions;
+using UnityEngine;
+using RWCustom;
+using BepInEx;
+using Debug = UnityEngine.Debug;
+using System.Drawing;
+
+
+
+namespace Plugin;
+
+public partial class DevTools
+{
+    public static void Init()
+    {
+        On.PlayerGraphics.DrawSprites += PlayerGraphics_DrawSprites;
+    }
+
+    private static void PlayerGraphics_DrawSprites(On.PlayerGraphics.orig_DrawSprites orig, PlayerGraphics self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, float timeStacker, Vector2 camPos)
+    {
+        orig(self, sLeaser, rCam, timeStacker, camPos);
+        sLeaser.sprites[2].color = UnityEngine.Color.red;
+    }
+
+    private void Terminate()
+    {
+
+    }
+
+    public static void Log(string message)
+    {
+        UnityEngine.Debug.Log($"[Palette] {message}");
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Security;
+using System.Security.Permissions;
+using UnityEngine;
+using RWCustom;
+using BepInEx;
+using Debug = UnityEngine.Debug;
+using DevInterface;
+
+#pragma warning disable CS0618
+
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+
+namespace Plugin;
+
+[BepInPlugin(ID, NAME, VER)]
+public partial class Plugin : BaseUnityPlugin 
+{
+    public const string ID = "skeq.rainstates";
+    public const string NAME = "Rain States";
+    public const string VER = "0.0.1";
+    private void OnEnable()
+    {
+        DevTools.Init();
+        //On.RainWorld.OnModsInit += RainWorldOnOnModsInit;
+    }
+
+    private void OnDisable()
+    {
+    }
+
+    /* Do this when the mod is finish?
+    private bool IsInit;
+    private void RainWorldOnOnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+    {
+        orig(self);
+        if (IsInit) return;
+
+        try
+        {
+            IsInit = true;
+
+            //Your hooks go here
+            
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex);
+        }
+    }
+    */
+
+}
